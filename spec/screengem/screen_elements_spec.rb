9dbf65149a3f81aef36b5ae0f1e34ddc59e60a82
Alistair McKinnell <alistairm@nulogy.com>
2019-08-20 14:03:30 -0400

    Address rubocop violations

--- spec/screengem/screen_elements_spec.rb
+++ spec/screengem/screen_elements_spec.rb
@@ -1,28 +1,30 @@
 module Screengem
   RSpec.describe ScreenElements do
+    # rubocop:disable RSpec/LeakyConstantDeclaration
     Module.new do
       # Does not override the visit_path method
-      ExampleOne = Class.new(Screengem::ScreenElement)
+      ScreenElementExampleOne = Class.new(Screengem::ScreenElement)
 
       # Does override the visit_path method
-      ExampleTwo = Class.new(Screengem::ScreenElement) do
+      ScreenElementExampleTwo = Class.new(Screengem::ScreenElement) do
         def visit_path
           "some/path"
         end
       end
     end
+    # rubocop:enable RSpec/LeakyConstantDeclaration
 
     subject(:screen) { Screengem::ScreenElements.instance }
 
     it "creates screen element methods" do
       generated_creation_methods = screen.public_methods(false)
 
-      expect(generated_creation_methods).to contain_exactly(:example_one, :example_two)
+      expect(generated_creation_methods).to contain_exactly(:screen_element_example_one, :screen_element_example_two)
     end
 
     it "wraps as expected" do
-      expect(screen.example_one).to be_a_kind_of(Screengem::ScreenElement)
-      expect(screen.example_two).to be_a_kind_of(Screengem::AutomaticVisit)
+      expect(screen.screen_element_example_one).to be_a_kind_of(Screengem::ScreenElement)
+      expect(screen.screen_element_example_two).to be_a_kind_of(Screengem::AutomaticVisit)
     end
   end
 end
