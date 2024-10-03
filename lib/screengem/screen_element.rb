b19b8b9f327043b8872f63f0981e23b9d2825887
Alistair McKinnell <alistairm@nulogy.com>
2024-10-03 19:58:23 -0400

    Better backwards compatibility with previous behaviour

--- lib/screengem/screen_element.rb
+++ lib/screengem/screen_element.rb
@@ -25,14 +25,17 @@ module Screengem
 
     #
     # Screen elements may define a visit_path that includes these elements
-    # of a URI: the path, a query string, and a fragment. When deciding to
-    # skip the page.visit we have to consider all of these elements.
+    # of a URI: the path, a query string, and a fragment. We consider only
+    # the path and the fragment when building the current_uri.
+    #
+    # This has the somewhat surprising consequence that a visit_path that contains
+    # a query string will never match the current_uri meaning that we will always
+    # auto visit. This preserves the behaviour in versions prior to 0.21.x.
     #
     def current_uri
       uri = URI(page.current_url)
 
       current_uri = uri.path
-      current_uri += "?#{uri.query}" if uri.query.present?
       current_uri += "##{uri.fragment}" if uri.fragment.present?
 
       current_uri
